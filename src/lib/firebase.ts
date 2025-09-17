d94d2e6497f8f69c7e7f106aae670bb752a2eb2f
nikhil jain <nikhiljain2391@gmail.com>
2025-09-17 19:41:23 +0000

    [2025-09-17T19:40:56.921Z]  @firebase/firestore: Firestore (11.9.0): Web

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAB785A_XtYTXJPonHrCHLYl8WD7fblglI",
   authDomain: "absolute-realm-470109-c9.firebaseapp.com",
   projectId: "absolute-realm-470109-c9",
-  storageBucket: "absolute-realm-470109-c9.firebasestorage.com",
+  storageBucket: "absolute-realm-470109-c9.appspot.com",
   messagingSenderId: "833486528271",
   appId: "1:833486528271:web:2d4d1168ad22ffead6687a",
   measurementId: "G-PTK02S4CV6"
