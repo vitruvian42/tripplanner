1eab481099b603796000dee8e4ee31f8284f5059
nikhil jain <nikhiljain2391@gmail.com>
2025-09-17 19:26:55 +0000

    npm run genkit:dev not working

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -3,18 +3,34 @@ config();
 
 import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/googleai';
+import { genkitEval } from '@genkit-ai/eval';
+import { dotprompt } from '@genkit-ai/dotprompt';
+
 
 // Import your flows here.
 import '@/ai/flows/ai-itinerary-generation.ts';
 import '@/ai/flows/ai-personal-trip-assistant.ts';
 
 genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI(),
+    dotprompt(),
+    genkitEval({
+      judge: 'googleai/gemini-1.5-flash',
+      metrics: ['reasoning', 'accuracy'],
+    }),
+  ],
   // Log all traces to the local filesystem.
   traceStore: {
     provider: 'file',
     options: {
       path: './.genkit/traces',
+    },
+  },
+  flowStateStore: {
+    provider: 'file',
+    options: {
+      path: './.genkit/flow-states',
     },
   },
 });
