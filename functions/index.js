87688c1122288fc866ffc731f0c19c64c354bb91
nikhil jain <nikhiljain2391@gmail.com>
2025-09-19 10:17:43 +0000

    ReferenceError: require is not defined     at file:///home/user/studio/f

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,9 @@
-
-const {onRequest} = require("firebase-functions/v2/https");
-const {setGlobalOptions} = require("firebase-functions/v2");
+import {onRequest} from "firebase-functions/v2/https";
+import {setGlobalOptions} from "firebase-functions/v2";
 
 setGlobalOptions({maxInstances: 10});
 
 const server = await import("firebase-frameworks");
-exports.server = onRequest(async (request, response) => {
+export const ssrabsoluterealm470109c = onRequest(async (request, response) => {
   return server.handle(request, response);
 });
