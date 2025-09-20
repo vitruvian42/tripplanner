5b5320839086b8dfe4c3cfa11ee394dedc366ee8
Nikhil Jain <nikhiljain@Adityas-MacBook-Pro.local>
2025-09-20 17:47:20 +0530

    firebase deploy try

--- functions/index.js
+++ functions/index.js
@@ -8,10 +8,10 @@ firebaseAdmin.initializeApp();
 
 setGlobalOptions({maxInstances: 10});
 
-// const server = await import("firebase-frameworks");
-// export const ssrabsoluterealm470109c = onRequest(async (request, response) => {
-//   return server.handle(request, response);
-// });
+const server = await import("firebase-frameworks");
+export const ssrabsoluterealm470109c = onRequest(async (request, response) => {
+  return server.handle(request, response);
+});
 
 const corsOptions = {
   origin: '*', // how to allow all origins  
