2ca9e4cfb0d41e3e6ad119a0227087ccc13b99c8
vitruvian42 <nikhiljain2391@gmail.com>
2025-11-07 01:49:59 +0530

    photos fix

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
     ],
   },
   experimental: {
